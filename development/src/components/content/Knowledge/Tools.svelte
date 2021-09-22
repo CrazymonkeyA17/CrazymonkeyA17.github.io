610514a9680aa684144d45d1e5d4666bfada828f
Nikhil Malhotra <nikhil.malhotra42@gmail.com>
2021-09-21 22:31:53 -0400

    Introduced a tools section

---
+++ development/src/components/content/Knowledge/Tools.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { tools, maincolor } from "../../../constants";
+</script>
+
+<div class="titleblock">
+  <h1>Tools</h1>
+  <p>The current tools at my command, until I find better ones</p>
+</div>
+<div class="toolscontainer">
+  {#each tools as tool}
+    <div class="logocontainer">
+      <img style="--theme-color: {maincolor}" src={tool.img} alt="{tool.name} logo">
+    </div>
+  {/each}
+</div>
+
+<style>
+  .toolscontainer {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  .logocontainer {
+    width: 300px;
+    height: 300px;
+    display: flex;
+    margin: 10px;
+    padding: 5px;
+    border-style: solid;
+    border-width: 5px;
+    backdrop-filter: drop-shadow(2px 4px 6px black);
+  }
+  img {
+    object-fit: contain;
+    width: 100%;
+    object-fit: contain;
+    width: 100%;
+    background-color: #0000001a;
+    filter: drop-shadow(2px 4px 6px black);
+  }
+
+</style>
--- development/src/constants.js
+++ development/src/constants.js
@@ -10,75 +10,119 @@ export const navbarmenu = ["ABOUT" ,"KNOWLEDGE", "CURRENT", "EXPERIENCE", "PROJE
 
 export const languages = [
   {
-    "name":"C++",
-    "level":3,
-    "comment":"I have never had to use this in a professional setting aside from a Co-op at Blackberry, originally coding in C++ 98. I'm updating this skill by going through a textbook into C++ 11/14. I'll jump into a personal project shortly after, probably something robot related.",
-    "img":"/img/cpluspluslogo.png"
+    name:"C++",
+    level:3,
+    comment:"I have never had to use this in a professional setting aside from a Co-op at Blackberry, originally coding in C++ 98. I'm updating this skill by going through a textbook into C++ 11/14. I'll jump into a personal project shortly after, probably something robot related.",
+    img:"./img/C++_logo.png"
   },
   {
-    "name":"Python",
-    "level":3,
-    "comment":"I started learning Python as I had heard a lot about it by word of mouth. This was quite a fun and straight forward language. It definitely pushes and promotes good coding practices",
-    "img":"/img/pngaaa.com-619138.png"
+    name:"Python",
+    level:3,
+    comment:"I started learning Python as I had heard a lot about it by word of mouth. This was quite a fun and straight forward language. It definitely pushes and promotes good coding practices",
+    img:"./img/Python_Logo.png"
   },
   {
-    "name":"Javascript",
-    "level":3,
-    "comment":"I got into Javascript by accident through a co-op where I needed to generate a mvp website. I didn't come back to it until recently when I was exploring the best way to interface with AutoCAD. I'm glad I came back because it looks like I didn't learn enough.",
-    "img":"/img/JavaScript-logo.png"
+    name:"Javascript",
+    level:3,
+    comment:"I got into Javascript by accident through a co-op where I needed to generate a mvp website. I didn't come back to it until recently when I was exploring the best way to interface with AutoCAD. I'm glad I came back because it looks like I didn't learn enough.",
+    img:"./img/JS_Logo.png"
   },
   {
-    "name":"G-Code",
-    "level":4,
-    "comment":"I ended up learning this at my first role as a Project Engineer. I kept on losing my manufacturing techs as a resource so I ended up having to fix and generate new CNC programs. This language is quite simliar to Assembly and SVG. The language has general codes that all machines understand and machine specific codes that only the machine being worked on will understand.",
-    "img":"/img/GCODE_Logo.png"
+    name:"G-Code",
+    level:4,
+    comment:"I ended up learning this at my first role as a Project Engineer. I kept on losing my manufacturing techs as a resource so I ended up having to fix and generate new CNC programs. This language is quite simliar to Assembly and SVG. The language has general codes that all machines understand and machine specific codes that only the machine being worked on will understand.",
+    img:"./img/GCODE_Logo.png"
   },
   {
-    "name":"AutoLISP",
-    "level":4,
-    "comment":"Why someone thought this language should exist as part of the AutoCAD suite baffles me. It is quite simple but too simple I am quite glad that there are other options as the brackets really got to me after a while of coding in the built in IDE, I got severe VBA PTSD flashbacks.",
-    "img":"/img/LISP_Logo.png"
+    name:"AutoLISP",
+    level:4,
+    comment:"Why someone thought this language should exist as part of the AutoCAD suite baffles me. It is quite simple but too simple I am quite glad that there are other options as the brackets really got to me after a while of coding in the built in IDE, I got severe VBA PTSD flashbacks.",
+    img:"./img/LISP_Logo.png"
   },
   {
-    "name":"MATLAB",
-    "level":3,
-    "comment":"Most of my exposure to this language has been in University, manufacturing doesnt tend to do vector math very often and neither do I. Otherwise I enjoyed working in this language, no crazy quirks and quite powerful",
-    "img":"/img/pngegg.png"
+    name:"MATLAB",
+    level:3,
+    comment:"Most of my exposure to this language has been in University, manufacturing doesnt tend to do vector math very often and neither do I. Otherwise I enjoyed working in this language, no crazy quirks and quite powerful",
+    img:"./img/MATLAB_Logo.png"
   },
   {
-    "name":"SQL",
-    "level":2,
-    "comment":"Databases have been quite touch and go for me. I've never had a project that required a heavy use of SQL so my understanding of the limitations of the technology is limited (no pun intended). I'll end up implementing some kind of highscore system on this website to learn this in more depth.",
-    "img":"/img/SQL_Logo.png"
+    name:"SQL",
+    level:2,
+    comment:"Databases have been quite touch and go for me. I've never had a project that required a heavy use of SQL so my understanding of the limitations of the technology is limited (no pun intended). I'll end up implementing some kind of highscore system on this website to learn this in more depth.",
+    img:"./img/SQL_Logo.png"
   },
   {
-    "name":"Latex",
-    "level":2,
-    "comment":"A lot of my classmates back in University were using this language for resume building and research papers. I'll admit I wasnt too confident in myself so I stuck with my trusty Word. Recently I decided why not give it a shot with my latest resumes. I have to admit it is quite fun to watch my content just dance into different formats as I switch different variables. However I am probably going to try for a web based resume now.",
-    "img":"/img/LATEX_Logo.png"
+    name:"Latex",
+    level:2,
+    comment:"A lot of my classmates back in University were using this language for resume building and research papers. I'll admit I wasnt too confident in myself so I stuck with my trusty Word. Recently I decided why not give it a shot with my latest resumes. I have to admit it is quite fun to watch my content just dance into different formats as I switch different variables. However I am probably going to try for a web based resume now.",
+    img:"./img/LATEX_Logo.png"
   },
   {
-    "name":"HTML",
-    "level":3,
-    "comment":"Similiar to Javascript I learned when making the mvp website. I know enough to be dangerous at this point.",
-    "img":"/img/HTML_Logo.png"
+    name:"HTML",
+    level:3,
+    comment:"Similiar to Javascript I learned when making the mvp website. I know enough to be dangerous at this point.",
+    img:"./img/HTML_Logo.png"
   },
   {
-    "name":"CSS",
-    "level":3,
-    "comment":"Similiar to Javascript I learned when making the mvp website. I thought I knew enough but I'm now learning the toolsets that make css coding a bit easier.",
-    "img":"/img/CSS_Logo.png"
+    name:"CSS",
+    level:3,
+    comment:"Similiar to Javascript I learned when making the mvp website. I thought I knew enough but I'm now learning the toolsets that make css coding a bit easier.",
+    img:"./img/CSS_Logo.png"
   }
 ];
 
 export const tools = [
   {
-
+    name:"GIT",
+    comment: "I would say 'All hail Linus Trovalds' but version control is such an ubiquitous problem it is more like why was this not made on day one of programming! I use this tool pretty often but I'm still learning its full feature set, mostly when things go wrong :P.",
+    img: "./img/GIT_Logo.png"
   },
   {
-
+    name:"GITHub",
+    comment: "Where someone went 'Let's sell git as a service'",
+    img: "./img/GITHUB_Logo.png"
   },
   {
-    
+    name:"JIRA",
+    comment: "I ran into this tool in university and I do use it on the odd personal group projects, but I really saw it fly recently when I was trying to introduce it to my team. I was in charge of setting it up but we became a Microsoft house so now I'm stuck with Planner...send help",
+    img: "./img/JIRA_Logo.png"
+  },
+  {
+    name:"Confluence",
+    comment: "This tool was touch and go during Unversity, but I had a chance to use it during a co-op at Blackberry as part of my onboarding and I sometimes use the free version for personal group projects.",
+    img: "./img/CON_Logo.png"
+  },
+  {
+    name:"VSCode",
+    comment: "This tool was touch and go ",
+    img: "./img/VSC_Logo.png"
   }
+];
+
+export const frameworks = [
+  {
+    name:"Svelte",
+    comment: "This tool was touch and go ",
+    img: "./img/SVELTE_Logo.png"
+  },
+  {
+    name:"NodeJS",
+    comment: "This tool was touch and go ",
+    img: "./img/NJS_Logo.png"
+  },
+  {
+    name:"nGinX",
+    comment: "This tool was touch and go ",
+    img: "./img/NGINX_Logo.png"
+  },
+  {
+    name:"React",
+    comment: "This tool was touch and go ",
+    img: "./img/REACT_Logo.png"
+  },
+  {
+    name:"Angular",
+    comment: "This tool was touch and go ",
+    img: "./img/AJS_Logo.png"
+  },
 ];
